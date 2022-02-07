6225ec62df4731094a579d821121f51411872688
saumya-mendiratta <saya.mendiratta@gmail.com>
2022-02-07 11:26:31 +0530

    Done without styling

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from .views import TaskList , TaskDetail , TaskCreate , TaskUpdate , TaskDelete 
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    
     path('login/' , CustomLoginview.as_view() , name = 'login'),
     path('logout/' , LogoutView.as_view(next_page = 'login') ,name = 'logout'),
     path('register/' , RegisterPage.as_view() ,name = 'register' ),
